4b6154cb8fd46effa5c8c8597d16e835668b91c3
lanlan513 <389357664@qq.com>
2026-09-13 14:21:39 +0800

    chore: assign unique ports to this branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5184,
     proxy: {
-      "/api": "http://localhost:8787"
+      "/api": "http://localhost:8794"
     }
   }
 });
